2ba53427d8a359310e61c541d5145ffa0b85a183
st148985 <st148985@stud.uni-stuttgart.de>
2023-01-06 23:22:54 +0100

    Stepless radius adaption and masked-arraybugfix
    The stars' apertures' radii are stepless now and depend on the maximum radius, which is defined by the user.
    A bug caused by masked arrays was fixed by removing all rows from the astropy-table, that contain masked values.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from astropy.coordinates import Angle
 from astroquery.simbad import Simbad
 
 #User inputs!!!
-result_table = Simbad.query_object("M3") #Name des Objekts (Existenz evtl. auf https://simbad.u-strasbg.fr/simbad/sim-fid kontrollieren)
+result_table = Simbad.query_object("gam Cas") #Name des Objekts (Existenz evtl. auf https://simbad.u-strasbg.fr/simbad/sim-fid kontrollieren)
 no_of_sources = 1000 #Anzahl der Sterne in der dxf-Datei
-roi_diameter_deg = 0.25 #Sternbild ca. 10-20 Grad, Kugelsternhaufen 0.5-1 Grad
+roi_diameter_deg = 10 #Sternbild ca. 10-20 Grad, Kugelsternhaufen 0.1-0.25 Grad
 no_layers = 5 #Die Anzahl der Layer, welchen die Sterne je nach Helligkeit zugeordnet werden
 create_circles = True #Auf False setzen, falls anstatt Kreisen nur Punkte gezeichnet werden sollen
-initial_radius_dxf = 1.0 #Der Radius der Kreise in der initialen Layer. Wird in jeder weiteren Layer halbiert.
+max_radius_dxf = 0.5 #Der Radius des hellsten Sterns/der hellsten Quelle. Die Querschnittsflaeche aller weiteren Sterne wird auf die Querschnittsflaeche dieses Sterns normiert
 #End of user inputs!!!
 
 roi_center_ra = Angle(result_table['RA'][0] + 'h').to(u.degree).to_value()
@@ -50,10 +50,28 @@ search_string = ("SELECT TOP {} {} FROM {} "
 
 sources_table = gaia.tap.query(search_string)
 
+#Remove all masked values from the results
+if sources_table.has_masked_values:
+    sources_table.remove_rows(np.where([c.data for c in sources_table.mask.itercols()])[-1])
+
+#Ensure, that the number of sources is a multiple of the layer count
+if not (len(sources_table) % no_layers == 0):
+    sources_table = sources_table[0:(-(len(sources_table) % no_layers))]
+
+magnitude_column = sources_table['phot_g_mean_mag']
 magnitudes_list = []
-magnitude_row = sources_table['phot_g_mean_mag']
+if create_circles:
+    radii_list = []
+    max_magnitude_area = max_radius_dxf**2
+    max_magnitude = magnitude_column[0]
 for tmp_source in sources_table:
-    magnitudes_list.append(tmp_source['phot_g_mean_mag'])
+    tmp_magnitude = tmp_source['phot_g_mean_mag']
+    magnitudes_list.append(tmp_magnitude)
+    if create_circles:
+        magnitude_delta = max_magnitude - tmp_magnitude
+        tmp_magnitude_area = max_magnitude_area * (2.512**magnitude_delta)
+        tmp_radius_dxf = np.sqrt(tmp_magnitude_area)
+        radii_list.append(tmp_radius_dxf)
 
 hist, bin_edges = np.histogram(magnitudes_list, no_layers)
 
@@ -77,8 +95,8 @@ for bin_no in range(no_layers):
         layer_points.append(cartesian_date)
     cartesian_points_layerwise.append(layer_points)
 
-#norm the maximum distance of all points to a maximum of 1000
-desired_max_euclidean_distance = 1000
+#norm the maximum distance of all points to the desired maximum
+desired_max_euclidean_distance = 75
 for layer_ind in range(len(cartesian_points_layerwise)):
     for i in range(len(cartesian_points_layerwise[layer_ind])):
         cartesian_points_layerwise[layer_ind][i] = [-1 * desired_max_euclidean_distance * cartesian_points_layerwise[layer_ind][i][0] / max_euclidean_distance, desired_max_euclidean_distance * cartesian_points_layerwise[layer_ind][i][1] / max_euclidean_distance]
@@ -86,13 +104,14 @@ for layer_ind in range(len(cartesian_points_layerwise)):
 #Write the data to a dxf-File
 doc = ezdxf.new("R2010")
 msp = doc.modelspace()
+point_ind = 0
 for layer_ind in range(len(cartesian_points_layerwise)):
     tmp_layer_name = "Layer{}".format(layer_ind+1)
     doc.layers.add(name=tmp_layer_name)
     for point in cartesian_points_layerwise[layer_ind]:
         if create_circles:
-            msp.add_circle((point[0], point[1]), initial_radius_dxf, dxfattribs={"layer": tmp_layer_name})
+            msp.add_circle((point[0], point[1]), radii_list[point_ind], dxfattribs={"layer": tmp_layer_name})
+            point_ind = point_ind + 1
         else:
             msp.add_point((point[0], point[1]), dxfattribs={"layer": tmp_layer_name})
-    initial_radius_dxf = initial_radius_dxf / 2
 doc.saveas("star_image.dxf")
